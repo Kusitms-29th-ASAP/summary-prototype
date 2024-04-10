c596548857d8bd0cf508f55ebe1b2623eb7e1da6
Sim-km <simkm0824@naver.com>
2024-04-10 19:02:56 +0900

    initial commit

---
+++ src/main/kotlin/com/asap/summaryprototype/SummaryPrototypeApplication.kt
@@ -0,0 +1,11 @@
+package com.asap.summaryprototype
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SummaryPrototypeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SummaryPrototypeApplication>(*args)
+}
